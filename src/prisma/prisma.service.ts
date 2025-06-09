0b93b133299ac5552f0d1cf53d8adeb670b90ec4
MaxiProgram <maxiprogram@gmail.com>
2025-06-09 22:15:03 +0300

    Try Prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient, Prisma } from 'generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
