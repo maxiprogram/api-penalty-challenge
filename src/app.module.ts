58fc6a417b691a8b1d5a845b109737650ca4cb77
MaxiProgram <maxiprogram@gmail.com>
2025-06-12 18:46:09 +0300

    Add static

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,16 @@ import { GoogleSheetService } from './google-sheet/google-sheet.service';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaService } from './prisma/prisma.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     HttpModule,
     ConfigModule.forRoot({isGlobal: true}),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'static'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, GoogleSheetService, PrismaService],
